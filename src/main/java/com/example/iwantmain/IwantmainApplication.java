0b547dcc77466bcbb31b7dcd4014e06ea774394a
LOGOS125-GALAXY\qkrtj <qkrtjdrbs125@naver.com>
2023-07-07 08:35:43 +0900

    init commit

---
+++ src/main/java/com/example/iwantmain/IwantmainApplication.java
@@ -0,0 +1,13 @@
+package com.example.iwantmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwantmainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IwantmainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/iwantmain/IwantmainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iwantmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwantmainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
